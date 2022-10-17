fee2fafdb18b1a6c4487f2b6cc21aafbda2ddb02
Md. Ruhul Amin <hmruhul16.mbstu@gmail.com>
2022-10-17 12:30:16 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Create_Document from './components/docs/Create_Document';
+import Create_Document from './components/MainPage/BBA_Documents/Create_Document';
 import Header from './components/initialpage/Sidebar/Header';
 import Sidebar from './components/initialpage/Sidebar/sidebar';
 import Dashboard from './components/MainPage/Main/Dashboard';
-import FileUpload from './components/File/FileUpload';
-import ViewDocuments from './components/docs/ViewDocuments';
-import DocumentList from './components/docs/DocumentList';
-import PdfView from './components/docs/PdfView';
-import Pdfview2 from './components/docs/Pdfview2';
-import Catlist from './components/docs/Catlist';
+
+import ViewDocuments from './components/MainPage/BBA_Documents/ViewDocuments';
+import DocumentList from './components/MainPage/BBA_Documents/DocumentList';
+import PdfView from './components/MainPage/BBA_Documents/PdfView';
+import Pdfview2 from './components/MainPage/BBA_Documents/Pdfview2';
+ import Catlist from './components/MainPage/BBA_Documents/Catlist';
 function App() {
   return (
     <div className="App">
@@ -25,7 +25,7 @@ function App() {
 
 
         <Route path="/docs/add" element={<Create_Document />} />
-        <Route path="/fileupload" element={<FileUpload />} />
+        {/* <Route path="/fileupload" element={<FileUpload />} /> */}
         
         <Route path="/ViewDocuments/:id/:document_id" element={<ViewDocuments />}/>
         <Route path="/pdfview/:name/:recordId" element={<PdfView />}/>
